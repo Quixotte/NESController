0cb14b3ca4a8a6106defe0d14992d39c97141b37
Stef Janssen <steffos.janssen@gmail.com>
2014-10-18 22:29:32 +0200

    mychanges

---
+++ ConfigViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ConfigViewController.swift
+//  NESController2
+//
+//  Created by Stef Janssen on 18/10/14.
+//  Copyright (c) 2014 Stef Janssen. All rights reserved.
+//
+
+import UIKit
+
+class ConfigViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
