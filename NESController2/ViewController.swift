c2678fa552978772c57fe013d5a3a2e6582697c6
Stef Janssen <steffos.janssen@gmail.com>
2014-10-18 17:36:26 +0200

    bugfix

--- NESController2/ViewController.swift
+++ NESController2/ViewController.swift
@@ -36,12 +36,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     @IBAction func pressed(sender: AnyObject) {
         
         if let buttonType = ButtonTypes.fromRaw(sender.tag){
