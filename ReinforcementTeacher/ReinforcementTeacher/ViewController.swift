2d2f0ea3dc0ee72f60c6091e12d45c2dc45bab4b
Stef Janssen <steffos.janssen@gmail.com>
2014-10-23 20:26:25 +0200

    actual project made now

---
+++ ReinforcementTeacher/ReinforcementTeacher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReinforcementTeacher
+//
+//  Created by Stef Janssen on 23/10/14.
+//  Copyright (c) 2014 Stef Janssen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
